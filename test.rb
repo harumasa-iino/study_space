094dabfc61e977c8f433d5053896418aaeb826eb
Harumasa Iino <haru10masa18@gmail.com>
2024-06-14 15:06:03 +0900

    root

--- test.rb
+++ test.rb
@@ -1,28 +1,18 @@
-def bubble_sort(arr)
-    n = arr.length
-    (n-1).times do
-      (0...(n-1)).each do |i|
-        if arr[i] > arr[i + 1]
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]  # Swap elements
-        end
-      end
+def extract_and_calculate_square_roots(number_sequence)
+    raise ArgumentError, "Input must be a string of digits" unless number_sequence.is_a?(String) && number_sequence.match?(/^\d+$/)
+    raise ArgumentError, "Input must be at least 1000 digits long" if number_sequence.length < 1000
+  
+    (0..number_sequence.length - 5).each do |i|
+      sub_number = number_sequence[i, 5]
+      square_root = Math.sqrt(sub_number.to_i)
+      puts "The square root of #{sub_number} is #{square_root}"
     end
-    arr
   end
   
   # 使用例
-  arr = [5, 3, 8, 4, 2]
-  puts bubble_sort(arr)
-
-def reverse(arr)
-    reversed = []
-    n = arr.length
-    while n > 0
-      n -= 1
-      reversed << arr[n]
-    end
-    reversed
-end
-
-arr = [1,2,3,4,5]
-print reverse(arr)
+  number_sequence = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+                    "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+                    "123456789012345678901234567890123456789789012345678901234567890" +
+                    "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+  
+  p extract_and_calculate_square_roots(number_sequence)
