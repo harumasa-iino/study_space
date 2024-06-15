978ced4e3df12966d05e1277ad44383ea98b6776
Harumasa Iino <haru10masa18@gmail.com>
2024-06-15 20:50:50 +0900

    root

--- test.rb
+++ test.rb
@@ -1,7 +1,4 @@
-def extract_and_calculate_square_roots(number_sequence)
-    raise ArgumentError, "Input must be a string of digits" unless number_sequence.is_a?(String) && number_sequence.match?(/^\d+$/)
-    raise ArgumentError, "Input must be at least 1000 digits long" if number_sequence.length < 1000
-  
+def extract_and_calculate_square_roots(number_sequence)  
     (0..number_sequence.length - 5).each do |i|
       sub_number = number_sequence[i, 5]
       square_root = Math.sqrt(sub_number.to_i)
@@ -10,9 +7,6 @@ def extract_and_calculate_square_roots(number_sequence)
   end
   
   # 使用例
-  number_sequence = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
-                    "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
-                    "123456789012345678901234567890123456789789012345678901234567890" +
-                    "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+  number_sequence = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123451234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
   
   p extract_and_calculate_square_roots(number_sequence)
